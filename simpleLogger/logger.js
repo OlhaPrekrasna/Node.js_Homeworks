433f649c5ab62fd9172be9dabd25364bcb974d5f
Olha Prekrasna <prekrasnaya.olya@gmail.com>
2025-07-22 16:14:54 +0200

    Added and fixed 1 homework

--- simpleLogger/logger.js
+++ simpleLogger/logger.js
@@ -1,10 +1,18 @@
 const fs = require('fs');
 
+// Создаю функцию для отправки сообщений
 function logMessage(message) {
+
+  // Добавляю метки времени, чтобы сообщения можно было различать по времени
   const now = new Date();
-  const timestamp = now.toLocaleString();
+
+  // Здесь привожу время к формату строки
+  const timestamp = now.toLocaleString(); 
+
+// Переменная, чтобы сформировать вид сообщения
   const logEntry = `[${timestamp}] ${message}\n`;
 
+  // Создаю файл для записи сообщений в него, условие, если возникла ошибка
   fs.appendFile('log.txt', logEntry, (err) => {
     if (err) {
       console.error('Ошибка при записи в лог: ', err);
