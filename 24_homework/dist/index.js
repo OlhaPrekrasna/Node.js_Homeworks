fe5ed09e728b469ddc7fe81da1c3dc405aa864a1
Olha Prekrasna <prekrasnaya.olya@gmail.com>
2025-09-29 13:10:21 +0200

    Homework 24

--- 24_homework/dist/index.js
+++ 24_homework/dist/index.js
@@ -1,24 +1,21 @@
-// Import express
 import express from 'express';
-// Create app instance
 const app = express();
-// Middleware for parsing JSON in request body
+
 app.use(express.json());
-// GET route
+// GET
 app.get('/', (req, res) => {
-    res.send('Hello! This is a GET route');
+  res.send('Hello! This is a GET route');
 });
-// POST route
+// POST
 app.post('/data', (req, res) => {
-    const data = req.body;
-    res.json({
-        message: 'Data received!',
-        yourData: data,
-    });
+  const data = req.body;
+  res.json({
+    message: 'Data received!',
+    yourData: data,
+  });
 });
-// Start server
+
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server running at: http://localhost:${PORT}`);
+  console.log(`Server running at: http://localhost:${PORT}`);
 });
-//# sourceMappingURL=index.js.map
